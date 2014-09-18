b01a4524f7e8bafb9f493d56ced7043c1c21cef8
Marc-Andre Lemburg <mal@egenix.com>
2014-09-18 11:20:34 +0200

    Add loop tests that use a C function as operator.

--- test_loops.py
+++ test_loops.py
@@ -8,7 +8,7 @@
 
 """
 
-### Loops
+### Loops with Python operation
 
 op = lambda x: x*2
 seq = range(1000)
@@ -36,6 +36,37 @@ def generator_loop():
     return list(op(x) for x in seq)
 
 assert generator_loop() == result
+
+### Loops with C function operation
+
+import operator
+    
+op1 = operator.inv
+seq1 = range(1000)
+result1 = map(op1, seq1)
+
+def for_loop_c_operation():
+    l = seq1[:]
+    for i, x in enumerate(seq1):
+        l[i] = op1(x)
+    return l
+
+assert for_loop_c_operation() == result1
+
+def map_loop_c_operation():
+    return map(op1, seq1)
+
+assert map_loop_c_operation() == result1
+
+def list_comprehension_loop_c_operation():
+    return [op1(x) for x in seq1]
+
+assert list_comprehension_loop_c_operation() == result1
+
+def generator_loop_c_operation():
+    return list(op1(x) for x in seq1)
+
+assert generator_loop_c_operation() == result1
     
 ###
 
